6cc6b7dbbaecba9aa783d0620fd94f14043cefa6
Sousa Melo <hugo.sousamelo.sio@gmail.com>
2018-09-11 15:50:42 +0200

    Constructeur Heure

--- Heure.java
+++ Heure.java
@@ -3,4 +3,18 @@ public class Heure {
 	private int heures ;
 	private int minutes ;
 
+	public Heure(int heures, int minutes) {
+		if(heures < 0 || heures > 23){
+			this.heures = 0 ;
+		}
+		else {
+			this.heures = heures ;
+		}
+		if(minutes < 0 || minutes > 59){
+			this.minutes = 0 ;
+		}
+		else {
+			this.minutes = minutes ;
+		}
+	}
 }
